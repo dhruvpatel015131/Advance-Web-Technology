4ec12df54256e12746f6364aaf985af8396d81ce
dhruvpatel015131 <140480404+dhruvpatel015131@users.noreply.github.com>
2024-02-29 06:49:26 +0530

    Add files via upload

---
+++ P1/P1/DateAndTimeCalculation.Designer.cs
@@ -0,0 +1,241 @@
+﻿
+namespace P1
+{
+    partial class DateAndTimeCalculation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtDaysLeft = new System.Windows.Forms.TextBox();
+            this.txtShortTime = new System.Windows.Forms.TextBox();
+            this.txtLongTime = new System.Windows.Forms.TextBox();
+            this.txtGeneralDT = new System.Windows.Forms.TextBox();
+            this.txtShortDate = new System.Windows.Forms.TextBox();
+            this.txtLongDate = new System.Windows.Forms.TextBox();
+            this.txtFullDateAndTime = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtDaysLeft);
+            this.panel1.Controls.Add(this.txtShortTime);
+            this.panel1.Controls.Add(this.txtLongTime);
+            this.panel1.Controls.Add(this.txtGeneralDT);
+            this.panel1.Controls.Add(this.txtShortDate);
+            this.panel1.Controls.Add(this.txtLongDate);
+            this.panel1.Controls.Add(this.txtFullDateAndTime);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(144, 56);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(472, 301);
+            this.panel1.TabIndex = 0;
+            // 
+            // txtDaysLeft
+            // 
+            this.txtDaysLeft.Location = new System.Drawing.Point(214, 231);
+            this.txtDaysLeft.Name = "txtDaysLeft";
+            this.txtDaysLeft.Size = new System.Drawing.Size(229, 20);
+            this.txtDaysLeft.TabIndex = 13;
+            // 
+            // txtShortTime
+            // 
+            this.txtShortTime.Location = new System.Drawing.Point(214, 195);
+            this.txtShortTime.Name = "txtShortTime";
+            this.txtShortTime.Size = new System.Drawing.Size(229, 20);
+            this.txtShortTime.TabIndex = 12;
+            // 
+            // txtLongTime
+            // 
+            this.txtLongTime.Location = new System.Drawing.Point(214, 158);
+            this.txtLongTime.Name = "txtLongTime";
+            this.txtLongTime.Size = new System.Drawing.Size(229, 20);
+            this.txtLongTime.TabIndex = 11;
+            // 
+            // txtGeneralDT
+            // 
+            this.txtGeneralDT.Location = new System.Drawing.Point(214, 122);
+            this.txtGeneralDT.Name = "txtGeneralDT";
+            this.txtGeneralDT.Size = new System.Drawing.Size(229, 20);
+            this.txtGeneralDT.TabIndex = 10;
+            // 
+            // txtShortDate
+            // 
+            this.txtShortDate.Location = new System.Drawing.Point(214, 88);
+            this.txtShortDate.Name = "txtShortDate";
+            this.txtShortDate.Size = new System.Drawing.Size(229, 20);
+            this.txtShortDate.TabIndex = 9;
+            // 
+            // txtLongDate
+            // 
+            this.txtLongDate.Location = new System.Drawing.Point(214, 55);
+            this.txtLongDate.Name = "txtLongDate";
+            this.txtLongDate.Size = new System.Drawing.Size(229, 20);
+            this.txtLongDate.TabIndex = 8;
+            // 
+            // txtFullDateAndTime
+            // 
+            this.txtFullDateAndTime.Location = new System.Drawing.Point(214, 23);
+            this.txtFullDateAndTime.Name = "txtFullDateAndTime";
+            this.txtFullDateAndTime.Size = new System.Drawing.Size(229, 20);
+            this.txtFullDateAndTime.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(49, 238);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(99, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Days until new year";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(49, 198);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Short Time";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(49, 161);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Long Time";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(49, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "General Date and Time";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Short Date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Long Date";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fulll date and time";
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(197, 364);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(420, 364);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // DateAndTimeCalculation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.panel1);
+            this.Name = "DateAndTimeCalculation";
+            this.Text = "Date and Time";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDaysLeft;
+        private System.Windows.Forms.TextBox txtShortTime;
+        private System.Windows.Forms.TextBox txtLongTime;
+        private System.Windows.Forms.TextBox txtGeneralDT;
+        private System.Windows.Forms.TextBox txtShortDate;
+        private System.Windows.Forms.TextBox txtLongDate;
+        private System.Windows.Forms.TextBox txtFullDateAndTime;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
---
+++ P1/P1/DateAndTimeCalculation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P1
+{
+    public partial class DateAndTimeCalculation : Form
+    {
+        
+        public DateAndTimeCalculation()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+           try
+            {
+                DateTime dt = DateTime.Now;
+                txtFullDateAndTime.Text = dt.ToString("f");
+                txtLongDate.Text = dt.ToString("D");
+                txtShortDate.Text = dt.ToString("d");
+                txtGeneralDT.Text = dt.ToString("G");
+                txtLongTime.Text = dt.ToString("T");
+                txtShortTime.Text = dt.ToString("t");
+                int daysInYear = DateTime.IsLeapYear(dt.Year) ? 365 : 66;
+                txtDaysLeft.Text = (daysInYear - dt.DayOfYear).ToString();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error Occured! " + ex.Message);
+            }
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
