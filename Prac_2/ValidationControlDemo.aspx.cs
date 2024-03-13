55cbce329bb41f992f00e10233d4174094ec1bc9
dhruvpatel015131 <140480404+dhruvpatel015131@users.noreply.github.com>
2024-03-13 18:47:29 +0530

    Add files via upload

---
+++ Prac_2/ValidationControlDemo.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Practical_2
+{
+    public partial class ValidationDemo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CustomValidator1_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+
+        }
+
+     
+    }
+}
